f88f1555f3b4728a6651adad038afc598ba4e034
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-20 11:25:10 -0400

    Completed Tip Calculator 3

--- tipCalculator.js
+++ tipCalculator.js
@@ -29,6 +29,9 @@ console.log(totalAmount(100, 'good'));
 console.log(totalAmount(40, 'fair'));
 
 // Part 3
-// function splitAmount(billAmount, levelOfService, numberOfPeople) {
+function splitAmount(billAmount, levelOfService, numberOfPeople) {
+    return totalAmount(billAmount, levelOfService) / numberOfPeople
+}
 
-// }
+console.log(splitAmount(100, 'good', 5));
+console.log(splitAmount(40, 'fair', 2));

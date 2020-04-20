4265606c6dc46e0d1a069636e3002961fb4aad4d
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-20 11:05:08 -0400

    Madlib function completed.

---
+++ madlib.js
@@ -0,0 +1,7 @@
+function madlib(name, subject) {
+    return (`${name}'s favorite subject in school is ${subject}.`)
+}
+
+console.log(madlib("Anushka", "art"))
+console.log(madlib("Gerrit", "computer science"))
+console.log(madlib("Roger", "PE"))

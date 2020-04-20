59a5f777f4692a465afd29cad52e09d8d070d199
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-20 11:21:52 -0400

    Completed Tip Calculator 1 and 2

---
+++ tipCalculator.js
@@ -0,0 +1,39 @@
+// Part 1
+let tip = 0;
+
+function tipAmount(billAmount, levelOfService) {
+    if (levelOfService === "good") {
+        tip = billAmount * .20;
+        return tip;
+    }
+    else if (levelOfService === "fair") {
+        tip = billAmount * .15;
+        return tip;
+    }
+    else if (levelOfService === "bad") {
+        tip = billAmount * .10;
+        return tip;
+    }
+}
+
+console.log(tipAmount(100, 'good'));
+console.log(tipAmount(40, 'fair'));
+
+//Part 2
+function totalAmount(billAmount, levelOfService) {
+    if (levelOfService === "good") {
+        tipAmount(billAmount, levelOfService)
+        return tip + billAmount;
+    }
+    else if (levelOfService === "fair") {
+        tipAmount(billAmount, levelOfService)
+        return tip + billAmount;
+    }
+    else if (levelOfService === "bad") {
+        tipAmount(billAmount, levelOfService)
+        return tip + billAmount;
+    }
+}
+
+console.log(totalAmount(100, 'good'));
+console.log(totalAmount(40, 'fair'));
